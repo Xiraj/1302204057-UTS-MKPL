376b09ef402caa266097c37b52dc4196c6e2d602
Xiraj <akbarsirajuddinh@gmail.com>
2023-04-14 21:06:27 +0700

    remake setMonthlySalary

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -33,26 +33,16 @@ public class Employee {
 		childIdNumbers = new LinkedList<String>();
 	}
 	
-	public void setMonthlySalary(int grade) {	
-		if (grade == 1) {
-			monthlySalary = 3000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
-		}else if (grade == 2) {
-			monthlySalary = 5000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
-		}else if (grade == 3) {
-			monthlySalary = 7000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
+	public void setMonthlySalary(int grade) {
+		int[] salaries = {3000000, 5000000, 7000000};
+		monthlySalary = salaries[grade - 1];
+		if (isForeigner) {
+			monthlySalary = (int) (monthlySalary * 1.5);
 		}
 	}
 	
-	public void setAnnualDeductible(int deductible) {	
+	
+	public void setAnnualDeductible(int deductible) {
 		this.annualDeductible = deductible;
 	}
 	
@@ -70,7 +60,7 @@ public class Employee {
 	}
 	
 	public int getAnnualIncomeTax() {
-	
+		
 		LocalDate date = LocalDate.now();
 		
 		if (date.getYear() == yearJoined) {
