8378368f25b874b211c320cc67779a898d796687
Xiraj <akbarsirajuddinh@gmail.com>
2023-04-14 21:25:50 +0700

    make effiency calculateTax

--- src/lib/TaxFunction.java
+++ src/lib/TaxFunction.java
@@ -3,29 +3,12 @@ package lib;
 public class TaxFunction {
 	
 	public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
-		
-		int tax = 0;
-		
-		if (numberOfMonthWorking > 12) {
-			System.err.println("More than 12 month working per year");
-		}
-		
-		if (numberOfChildren > 3) {
-			numberOfChildren = 3;
-		}
-		
-		if (isMarried) {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - (54000000 + 4500000 + (numberOfChildren * 1500000))));
-		}else {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - 54000000));
-		}
-		
-		if (tax < 0) {
-			return 0;
-		}else {
-			return tax;
-		}
-			 
+		int maxChildren = 3;
+		int taxDeduction = 54000000 + 4500000 + (Math.min(numberOfChildren, maxChildren) * 1500000);
+		int totalIncome = (monthlySalary + otherMonthlyIncome) * numberOfMonthWorking - deductible - taxDeduction;
+		int tax = (int) Math.round(0.05 * totalIncome);
+		return Math.max(tax, 0);
 	}
+	
 	
 }
